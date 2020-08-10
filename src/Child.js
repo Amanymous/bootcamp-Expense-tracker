a2cd53f227214ef89371d23b1834374e72911bcd
aman mirza <aman mirza99>
2020-08-10 22:49:09 +0500

    simple child.js layout and app.css

--- src/Child.js
+++ src/Child.js
@@ -15,6 +15,53 @@ export function Child() {
           Amanymous
         </a>
       </h2>
+      <h3>
+        Your Balance <br />
+        $300
+      </h3>
+      <div className="expense-cont">
+        <h3>
+          INCOME <br />
+          $1000
+        </h3>
+        <h3>
+          EXPENSE <br />
+          $500
+        </h3>
+      </div>
+      <h3>History</h3>
+      <hr />
+      <ul className="transaction-list">
+        <li>
+          <span>Cash</span>
+          <span>+400</span>
+        </li>
+        <li>
+          <span>Cash</span>
+          <span>+400</span>
+        </li>{' '}
+        <li>
+          <span>Cash</span>
+          <span>+400</span>
+        </li>{' '}
+        <li>
+          <span>Cash</span>
+          <span>+400</span>
+        </li>
+      </ul>
+      <form className=" ">
+        <label>
+          Enter Description <br />
+          <input type="text" required />
+        </label>
+        <br />
+        <label>
+          Enter Amount <br />
+          <input type="number" required />
+        </label>
+        <br />
+        <input type="submit" value="Add Your Transaction" />
+      </form>
     </div>
   );
 }
